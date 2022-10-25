3747e4d047c04d167a9b94df75a233b5283fb236
mondiu <giansebas.2015@gmail.com>
2022-10-25 17:25:20 -0500

    Primer mensaje

---
+++ src/modules/daybook/components/EntryItem.vue
@@ -0,0 +1,37 @@
+<template>
+    <div 
+    class="entry-container mb-3 pointer p-2"
+    @click="$router.push({name: 'entry', params: {id:10} })"
+    >
+        <div class="entry-title d-flex">
+            <span class="text-success fs-5 fw-bold">24</span>
+            <span class="mx-1 fs-5">Octubre</span>
+            <span class="mx-1 fw-light">2022, Lunes</span>
+
+        </div>
+        <div class="entry-description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, rem doloremque fugit quae consectetur iure vel voluptatibus provident nam explicabo aut quaerat optio soluta pariatur eaque a perspiciatis natus molestiae!
+        </div>
+
+    </div>
+    
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="scss" scoped>
+.entry-container{
+    border-bottom: 1px solid #2c3e50;
+    transition: 0.2s all ease-in;
+    &:hover{
+        background-color: lighten($color: grey, $amount: 0);
+        transition: 0.2s all ease-in;
+    }
+    .entry-description{
+        font-size: 12px;
+    }
+}
+
+</style>
---
+++ src/modules/daybook/views/EntryView.vue
@@ -0,0 +1,66 @@
+<template>
+<div class="entry-title d-flex justify-content-content-between p-2">
+    <div>
+        <span class="text-success fs-3 fw-bold">25</span>
+        <span class="mx-1 fs-3">Octubre</span>
+        <span class="mx-2 fs-4 fw-light">2022, Martes</span>
+
+
+    </div>
+    <div>
+    <button class="btn btn-danger mx-2">
+        Borrar
+        <i class="fa fa-trash-alt"></i>
+    </button>
+    <button class="btn btn-primary">
+        Subir Foto
+        <i class="fa fa-upload"></i>
+    </button>
+    </div>
+
+</div>
+    <hr>
+    <div class="d-flex flex-column px-3 h-75">
+        <textarea
+            placeholder="¿Que sucedio hoy?"
+        ></textarea>
+    </div>
+    <FabItem
+        icon="fa-save"
+    />
+    <img src="https://pbs.twimg.com/media/Eeb4Kf9XkAA2t1t.jpg" alt="entry-pictyre" class="img-thumbnail">
+
+
+</template>
+
+<script>
+import { defineAsyncComponent } from 'vue'
+export default {
+    components: {
+        FabItem: defineAsyncComponent( ()=> import('../components/FabItem.vue') )
+    }
+
+}
+</script>
+
+<style lang="scss" scoped>
+textarea {
+    font-size: 20px;
+    border: none;
+    height: 100%;
+
+    &:focus {
+        outline: none;
+
+    }
+    
+}
+img{
+        bottom: 150px;
+        box-shadow: 0px 5px 10px rgba($color: #000000, $alpha: 0.2);
+        position: fixed;
+        right: 20px;
+        width: 200px;
+    }
+
+</style>
